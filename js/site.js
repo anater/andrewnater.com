3b93e9d36f5f0ff561e77bf8bf5688d60e2aa813
Andrew Nater <a.a.nater@gmail.com>
2015-09-29 14:01:47 -0400

    Implement JS for project preview

---
+++ js/site.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+
+	var projectList = document.querySelector(".project-list");
+	var listItems = projectList.querySelectorAll(".item");
+	var projectPreview = document.querySelector(".project-preview");
+	var previewItems = projectPreview.querySelectorAll(".img-preview");
+
+	for (var i = 0; i < listItems.length; i++) {
+		listItems[i].querySelector('a').addEventListener('mouseenter', function(){
+			var projectCode = this.getAttribute("data-item");
+			for (var i = 0; i < previewItems.length; i++) {
+				var previewCode = previewItems[i].getAttribute("data-item");
+				console.log(previewCode);
+				if(previewCode == projectCode){
+					previewItems[i].classList.add("-selected");
+					projectPreview.classList.add("-" + String(previewCode));
+				}else{
+					previewItems[i].classList.remove("-selected");
+					projectPreview.classList.remove("-" + String(previewCode));
+				}
+			};
+		});
+	};
+
+};
