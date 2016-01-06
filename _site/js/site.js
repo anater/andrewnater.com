b88a63d48b90f4ccb6811c480c01eef0c7cc62fd
Andrew Nater <a.a.nater@gmail.com>
2016-01-06 17:58:16 -0500

    Integrate GA. Add event tracking to links.

--- _site/js/site.js
+++ _site/js/site.js
@@ -6,16 +6,13 @@ window.onload = function(){
 	var previewItems = projectPreview.querySelectorAll(".img-preview");
 
 	for (var i = 0; i < listItems.length; i++) {
-
+		listItems[i].querySelector('a').addEventListener('click', function(){
+			ga('send', 'event', 'Project', 'click', this.getAttribute("data-item"));
+		});
 		listItems[i].querySelector('a').addEventListener('mouseenter', function(){
-
 			var projectCode = this.getAttribute("data-item");
-
 			for (var i = 0; i < previewItems.length; i++) {
-
 				var previewCode = previewItems[i].getAttribute("data-item");
-				console.log(previewCode);
-
 				if(previewCode == projectCode){
 					previewItems[i].classList.add("-selected");
 					projectPreview.classList.add("-" + String(previewCode));
@@ -23,11 +20,8 @@ window.onload = function(){
 					previewItems[i].classList.remove("-selected");
 					projectPreview.classList.remove("-" + String(previewCode));
 				}
-
 			};
-
 		});
-
 	};
 
 };
