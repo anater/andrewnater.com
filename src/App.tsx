32b499b7eb3a7da9cfbca28c6644d6500a12dc71
Andrew Nater <a.a.nater@gmail.com>
2018-12-30 20:57:56 -0500

    Add header and page rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import * as React from "react";
+import { Helmet } from "react-helmet";
 
-export default class App extends React.PureComponent {
-    render () {
-        return <div>test</div>;
+import Default from "./layouts/Default";
+
+export default class App extends React.PureComponent<any> {
+  render() {
+    return (
+      <>
+        {this.renderHeader()}
+        {this.renderPage()}
+      </>
+    );
+  }
+
+  renderHeader = () => (
+    <Helmet>
+      <title>{this.props.title}</title>
+      <meta name="description" content={this.props.description} />
+      <body className="sans-serif" />
+    </Helmet>
+  );
+
+  renderPage = () => {
+    const { type, content } = this.props;
+    // choose layout
+    switch (type) {
+      case "Default":
+      default:
+        return <Default content={content} />;
     }
+  };
 }
