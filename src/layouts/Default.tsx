6ed4f45173535554c7e2eda410837f50fd21dbdb
Andrew Nater <a.a.nater@gmail.com>
2018-12-30 20:59:31 -0500

    Add Default layout

---
+++ src/layouts/Default.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import * as Contentful from "contentful";
+
+import Markdown from "../components/Markdown";
+
+interface Props {
+  content: Array<ContentItem>;
+}
+
+interface ContentItem {
+  sys: Contentful.Sys;
+  fields: any;
+}
+
+export default class Default extends React.PureComponent<Props> {
+  render() {
+    const { content } = this.props;
+    const date = new Date();
+    const year = date.getFullYear();
+    return (
+      <main className="measure-wide center">
+        {content.length > 0 && content.map(this.renderContentItem)}
+        <footer className="tc f6 mb5">Copyright Andrew Nater {year}</footer>
+      </main>
+    );
+  }
+
+  renderContentItem = ({ sys, fields }: ContentItem) => {
+    const { id, contentType } = sys;
+    const type = contentType.sys.id;
+
+    switch (type) {
+      case "text":
+        // render text
+        return (
+          <section key={id}>
+            {fields.showTitle && <h1>{fields.title}</h1>}
+            <Markdown content={fields.body} />
+          </section>
+        );
+      case "group":
+        // render group
+        return (
+          <section key={id}>
+            {fields.showTitle && <h1>{fields.title}</h1>}
+            {fields.items && fields.items.map(this.renderContentItem)}
+          </section>
+        );
+      default:
+        return false;
+    }
+  };
+}
