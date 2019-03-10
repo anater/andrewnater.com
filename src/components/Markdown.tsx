ecdf364ddfcd349d7493ab4d790850b0852d4044
Andrew Nater <a.a.nater@gmail.com>
2019-03-10 13:49:01 -0400

    Integrate highlight.js with `Markdown` component

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,13 +1,25 @@
 import * as React from "react";
 import * as MarkdownIt from "markdown-it";
+import * as hljs from "highlight.js";
 
 interface Props {
   content: string;
 }
 
-const markdownIt = new MarkdownIt();
+const markdownIt = new MarkdownIt({
+  highlight: (string: string, language: string): string => {
+    if (language && hljs.getLanguage(language)) {
+      try {
+        return hljs.highlight(language, string).value;
+      } catch (_) {
+        throw new EvalError("Highlighting failed");
+      }
+    }
+    return "";
+  }
+});
 
 export default ({ content }: Props) => {
   const __html = markdownIt.render(content);
-  return <div dangerouslySetInnerHTML={{ __html }} />
+  return <div dangerouslySetInnerHTML={{ __html }} />;
 };
